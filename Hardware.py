631d3b6de2b7ee26d55b59ea526c701de7adfb26
PimentaMateus <mateus.pimenta@pucpr.edu.br>
2019-11-15 18:25:33 -0300

    Terreno Preparado

---
+++ Hardware.py
@@ -0,0 +1,1 @@
+#Aqui vamos fazer toda a camada de hardware, tudo que entra de valores do sensor e tudo que sai para o LCD e Buzzer.
---
+++ Main.py
@@ -0,0 +1,3 @@
+#Aqui vamos fazer toda a execução padrao, como:
+# Comunicação com cayenne
+#Processamento dos valores de entrada e saída etc
