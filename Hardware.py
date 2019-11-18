ddd202a1239a2494d2e9bfa3e06e907504d04467
PimentaMateus <mateus.pimenta@pucpr.edu.br>
2019-11-18 12:06:40 -0300

    Adicionado teste do sensor

--- Hardware.py
+++ Hardware.py
@@ -1,2 +1,21 @@
-#Aqui vamos fazer toda a camada de hardware, tudo que entra de valores do sensor e tudo que sai para o LCD e Buzzer.
-# import RPi.GPIO as gpio **essa biblioteca só funciona em linux**
+'Aqui vamos fazer toda a camada de hardware, tudo que entra de valores do sensor e tudo que sai para o LCD e Buzzer.'
+import RPi.GPIO as gpio
+import time
+pinoSom = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pinoSom, GPIO.IN)
+
+#Leitura do sensor de som
+def detec(pinoSom):
+    if GPIO.input(pinoSom):
+        print("som...som")
+    else:
+        print("sem som...")
+
+
+GPIO.addEventoDetec(pinoSom, GPIO.BOTH, bouncetime=300)
+GPIO.addEventoCallBack(pinoSom, detec)
+
+
+while True:
+    time.sleep(1)
--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 # Aqui vamos fazer toda a execução padrao, como:
 # Comunicação com cayenne
 # Processamento dos valores de entrada e saída etc
-
-# import RPi.GPIO as gpio **essa biblioteca só funciona em linux**
+import RPi.GPIO as gpio
+import time
