73dc7c3e29a09621fdfc13f29b0539c6e8b6f2a2
Felipe Tomazelli de Lima <fefetl08@gmail.com>
2019-11-21 20:47:21 -0300

    versao1.0 cayenne

--- Main.py
+++ Main.py
@@ -2,4 +2,26 @@
 # Comunicação com cayenne
 # Processamento dos valores de entrada e saída etc
 import RPi.GPIO as gpio
+import paho.mqtt.client as mqtt
 import time
+import hardware as hd
+
+user = '7f730680-0734-11ea-b49d-5f4b6757b1bf'
+password = 'dfb9be8e000efe3b25a1ae8885c4e16e0f2708e7'
+client_id = '3718a860-0737-11ea-a38a-d57172a4b4d4'
+server = 'mqtt.mydevices.com'
+port = 1883
+
+# Client ID
+client = mqtt.Client(client_id)
+
+# MQTT username, MQTT password
+client.username_pw_set(user, password)
+
+# MQTT Server, MQTT Port,
+client.connect(server, port)
+
+# Envia a informacao
+client.publish('v1/' + user + '/things/' + client_id + '/data/0', hd.som())
+client.publish('v1/' + user + '/things/' + client_id + '/data/1', hd.led())
+time.sleep(5)
