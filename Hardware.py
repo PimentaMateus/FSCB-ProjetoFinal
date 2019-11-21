e3fd0a1de4d159cf8c3f118251a0af3f56cb8fa7
PimentaMateus <mateus.pimenta@pucpr.edu.br>
2019-11-21 20:54:17 -0300

    Versao 1.2 - Leitura de sensores

--- Hardware.py
+++ Hardware.py
@@ -1,9 +1,25 @@
-def som(): # decibeis 
-    return 120
+import RPi.GPIO as GPIO
+import time
+
+channel = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+nivel = GPIO.input(channel)
+def som(nivel): # decibeis
+
+    GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+    GPIO.add_event_callback(channel, som)  # assign function to GPIO PIN, Run function on change
+    return nivel
 
 
 def led(): # ai liga on/off no cayanne
-    if som() > 100:
+    if som(nivel) > 100:
         return 'ON'
     else:
         return 'OFF'
+
+#!/usr/bin/python
+
+
+#GPIO SETUP
+
